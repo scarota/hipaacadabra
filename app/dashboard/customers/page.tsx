6a739c72c153646d851f6da5c633364b6b852ceb
Sylvio Carota <scarota@revolutionehr.com>
2024-12-21 11:50:13 -0800

    latest changes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -6,14 +6,15 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
-}) {
+export default async function Page(
+  props: {
+    searchParams?: Promise<{
+      query?: string;
+      page?: string;
+    }>;
+  }
+) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
 
   return <CustomersTable query={query} />;
