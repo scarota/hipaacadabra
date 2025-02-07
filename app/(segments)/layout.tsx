bb132a67214d96cfa39ea5b93a239fe480a3ac40
Sylvio Carota <scarota@revolutionehr.com>
2025-02-07 09:17:49 -0800

    refactor

--- app/(segments)/layout.tsx
+++ app/(segments)/layout.tsx
@@ -6,9 +6,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-screen flex-col">
       <TopNav />
       <div className="flex flex-1">
-        <div className="w-64 border-r bg-white">
-          <SideNav />
-        </div>
         <div className="flex-1 overflow-y-auto bg-gray-50 p-8">{children}</div>
       </div>
     </div>
--- app/(segments)/settings/page.tsx
+++ app/(segments)/settings/page.tsx
@@ -1,3 +1,4 @@
+import SideNav from '@/app/ui/sidenav';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,10 +7,15 @@ export const metadata: Metadata = {
 
 export default function SettingsPage() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <p className="text-3xl font-bold">
-        This is a placeholder for the Settings page.
-      </p>
+    <div className="-m-8 flex h-full">
+      <div className="w-64 border-r bg-white">
+        <SideNav />
+      </div>
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-3xl font-bold">
+          This is a placeholder for the Settings page.
+        </p>
+      </div>
     </div>
   );
 }
